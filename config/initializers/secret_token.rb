5250e1ae6837ca2402b06f6145efcb6f5ba77777
Ian Grabill <Ian.Grabill@gmail.com>
2011-11-03 13:37:26 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GtmTwitter::Application.config.secret_token = '071c2941e5dd8361fa554f427ffed1c9b10db8368b34be99189e2fe8cb125ce3ab83a541d9bc1f36bd5eedf5829fd641cddfa5853a09c9b1f778fa23be2886f4'
